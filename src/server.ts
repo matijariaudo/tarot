44eeb7b75149b65a2aec7dbda913369a6346acc8
Mati Ariaudo <98933246+matijariaudo@users.noreply.github.com>
2023-11-10 13:09:24 +1000

    Cambios en package y server

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ const ServerStart=()=>{
     const privateKey = fs.readFileSync(path.join(__dirname, '../public/certificates/private-key.pem'), 'utf8');
     const certificate = fs.readFileSync(path.join(__dirname, '../public/certificates/public-cert.pem'), 'utf8');
     const credentials = { key: privateKey, cert: certificate };
-    connectDB();
+    //connectDB();
     const corsOptions:CorsOptions = {
       origin: function (origin:any, callback:any) {
         console.log("Origen:",origin,whiteList.indexOf(origin))
